f9677cb740388d98e7cdd62e6c4b6ea53b1c6a4b
Rex Herndon <herndonrex1@gmail.com>
2024-05-20 22:17:42 -0500

    fetched API from back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+    // state variable will contain our data from back-end API for re-rendering
+  const [backendData, setBackendData] = useState([{}])
+
+  // fetch back end data
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        // remember that since backendData is immutable, we set the new state of it with the setBackendData function
+        setBackendData(data)
+      }
+    )
+  }, []) // empty array specifies that this only runs on the first render of the component
+
+
   return (
     <div>App</div>
   )
